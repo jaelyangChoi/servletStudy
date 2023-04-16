c30a847d705c8a71e76fbaf9628ffc82f031ed59
Jealyang <jaelyangchoi@gmail.com>
2023-04-16 22:54:46 +0900

    HTTP 요청 데이터 조회 - POST HTML Form * request.getParameter() => GET URL 쿼리 파라미터 형식도 지원하고, POST HTML Form 형식도 지원 - application/x-www-form-urlencoded 형식은 GET의 쿼리 파라미터 형식과 같아 조회 메서드를 그대로 사용 가능.

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -8,9 +8,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 1. 파라미터 전송 기능
+ * 1. GET 파라미터 전송 기능
  * http://localhost:8080/request-param?username=hello&age=20
+ * 2. POST HTML Form 전송 기능
+ * application/x-www-form-urlencoded 형식은 앞서 GET에서 살펴본 쿼리 파라미터 형식과 같다.
+ * 따라서 쿼리 파라미터 조회 메서드를 그대로 사용하면 된다.
+ *
+ * !정리하면 request.getParameter() 는 GET URL 쿼리 파라미터 형식과 POST HTML Form 형식 둘 다 지원한다.
  */
+
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
     @Override
