5c9ca3794f795b5e427ded8f00b6972d358715c6
Jealyang <jaelyangchoi@gmail.com>
2023-04-18 00:35:32 +0900

    JSP와 Servlet을 이용하여 MVC 패턴으로 변경

---
+++ src/main/java/hello/servlet/web/serveltMvc/MvcMemberListServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.serveltMvc;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        //뷰로 포워딩하는 코드가 자꾸 반복된다.. 개선의 여지가 있다.
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
