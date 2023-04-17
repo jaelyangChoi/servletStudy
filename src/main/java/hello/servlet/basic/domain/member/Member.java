7064899873401c8cf566b01ed3d371a0ec08fc32
Jealyang <jaelyangchoi@gmail.com>
2023-04-17 22:04:15 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

--- src/main/java/hello/servlet/basic/domain/member/Member.java
+++ src/main/java/hello/servlet/basic/domain/member/Member.java
@@ -7,14 +7,14 @@ import lombok.Setter;
 @Setter
 public class Member {
     private Long id;
-    private String name;
+    private String username;
     private int age;
 
     public Member() {
     }
 
     public Member(String name, int age) {
-        this.name = name;
+        this.username = name;
         this.age = age;
     }
 }
