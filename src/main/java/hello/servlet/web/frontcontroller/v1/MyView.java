76344f878e9c9f5a3c70a1138a8ae5b6e6bd9a17
Jealyang <jaelyangchoi@gmail.com>
2023-04-24 22:45:48 +0900

    프론트 컨트롤러 도입 - v3 1. 서블릿 종속성 제거 : 컨트롤러 입장에서 필요없는 HttpServletRequest/Response 제거 -> 코드 단순, 테스트 용이. 이를 위해 model 생성. 2. 뷰 이름 중복 제거 : 뷰의 논리 이름만 반환하고 viewResolver에 변경 지점을 모은다.

--- src/main/java/hello/servlet/web/frontcontroller/v1/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private final String viewPath;
@@ -18,4 +19,14 @@ public class MyView {
         requestDispatcher.forward(request, response); //redirect가 아니라 서버 내부적으로 호출
     }
 
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request); //뭘 하는 건지 파악하기 쉽게 메소드로 추출하여 메소드명이 설명이 되게 한다!
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request, response); //redirect가 아니라 서버 내부적으로 호출
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        //JSP는 request.getAttribute()로 데이터를 조회하기 때문에 담아준다.
+        model.forEach((key, value) -> request.setAttribute(key, value));
+    }
 }
---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontContollerServletV3.java
@@ -0,0 +1,61 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v1.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@WebServlet(name = "frontContollerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontContollerServletV3 extends HttpServlet {
+
+    private final Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    public FrontContollerServletV3() {
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+
+        ControllerV3 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Map<String, String> paramMap = createParamMap(request); //위 아래 흐름에 비해 너무 디테일해. 레벨을 맞추기 위해 메소드로 추출
+        ModelView modelView = controller.process(paramMap);
+
+        String viewName = modelView.getViewName();
+        MyView myView = viewResolver(viewName); //위 아래 흐름에 비해 너무 디테일해. 레벨을 맞추기 위해 메소드로 추출
+
+        myView.render(modelView.getModel(), request, response);
+    }
+
+    private MyView viewResolver(String viewName) {
+        //변경의 지점을 한 곳으로 모은다!
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames()
+                .asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
