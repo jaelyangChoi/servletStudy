e9419d1e653565c3e10de94a75b9fe311664b974
Jealyang <jaelyangchoi@gmail.com>
2023-04-28 21:47:57 +0900

    프론트 컨트롤러 도입 - v5 유연한 컨트롤러 => 어댑터는 프론트 컨트롤러가 처리할 수 있는 형태로 형식을 맞추어 반환한다!

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -0,0 +1,44 @@
+package hello.servlet.web.frontcontroller.v5.adapter;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV4);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV4 controller = (ControllerV4) handler;
+
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>();
+
+
+        String viewName = controller.process(paramMap, model);
+
+        /**여기서 어댑터가 꼭 필요한 이유가 나온다.
+           ControllerV4 는 뷰의 이름을 반환했지만, 어댑터는 이것을 ModelView로 만들어서 형식을 맞추어 반환한다. */
+        ModelView modelView = new ModelView(viewName);
+        modelView.setModel(model);
+        return modelView;
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames()
+                .asIterator()
+                .forEachRemaining(param -> paramMap.put(param, request.getParameter(param)));
+        return paramMap;
+    }
+}
